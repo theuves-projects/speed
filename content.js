18be36b90e4ff4901b0daba7771dfd02025ed72b
Matheus Alves <theuves@gmail.com>
2019-03-07 12:24:09 -0400

    Primeiro commit

---
+++ content.js
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(function (req, _, sendResponse) {
+  switch (req) {
+    case 'GET_DEFAULT_PLAYBACK_RATE':
+      sendResponse({
+        defaultPlaybackRate: (function () {
+          var video = document.querySelector('video')
+
+          return video && typeof video.playbackRate === 'number'
+            ? video.playbackRate
+            : 1
+        })()
+      });
+      break
+    default:
+      sendResponse({})
+  }
+})
